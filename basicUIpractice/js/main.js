7d823575d54877ca0efc00aac3762dd4a1c479f9
Eri <izutsu812@gmail.com>
2020-04-06 19:35:20 +0900

    no message

--- basicUIpractice/js/main.js
+++ basicUIpractice/js/main.js
@@ -3,8 +3,17 @@
 	const accordion_wrapper = document.querySelectorAll('.accordion .wrapper');
 	const answer = document.querySelectorAll('.wrapper-txt_a');
 
+	for (let i = 0; i < accordion_wrapper.length; i++) {
+		accordion_wrapper[i].addEventListener('click', (e) => {
 
-	accordion_wrapper.addEventlistener('click', () => {
-		answer.classList.toggle('active');
-	});
+			const target = event.target
+			const target_answer = target.nextElementSibling;
+
+			console.log(target);
+			console.log(target_answer);
+			
+			target_answer.classList.toggle('active');
+		});
+	}
+
 }
