8b048c70f3681989eeae746340ac9958499f5e44
Eri <izutsu812@gmail.com>
2020-04-19 18:39:32 +0900

    no message

--- basicUIpractice/sickyHeader/pattern2/js/main.js
+++ basicUIpractice/sickyHeader/pattern2/js/main.js
@@ -1,19 +1,33 @@
 'use strict';
 {
 	const header = document.getElementById('header');
+	const headerChild = header.children[0]; //子要素を取得
+	const headerChildClone = headerChild.cloneNode(true); //子要素を複製
+	const headerClone = document.createElement('div'); //空のdivを作成
+	const body = document.querySelector('body');
 	const rect = header.getBoundingClientRect();
 	const scrollY = window.scrollY; // ページトップからのスクロール量
 	const headerOffsetTop = rect.top + scrollY; // rect.top : ビューポートにおけるページトップからの距離
-	console.log(scrollY);
-	console.log(headerOffsetTop);
+
+	//------------------------------------------
+	// headerCloneの生成
+	//------------------------------------------
+
+	headerClone.classList.add('header-clone'); //divにクラス名をつける
+	headerClone.appendChild(headerChildClone); //divにheaderChildを挿入する
+	body.appendChild(headerClone);
+
+	//------------------------------------------
+	// ヘッダーを切り替える処理
+	//------------------------------------------
 
 	//ウィンドウのスクロールイベントを監視
-	//スクロールするごとに処理を実行→window.scrollYの量はスクロールするたびに更新される
+	//スクロールするごとに処理を実行
 	function onScrollHandler () {
 		if (window.scrollY > headerOffsetTop) {
-			header.classList.add('sticky');
+			headerClone.style.top = '0';
 		} else {
-			header.classList.remove('sticky');
+			headerClone.style.top = '-35px';
 		}
 	}
 
