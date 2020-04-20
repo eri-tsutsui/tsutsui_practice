911e9a52b3439382362acb01bf1eadb06ebde544
Eri <izutsu812@gmail.com>
2020-04-20 11:55:08 +0900

    no message

--- basicUIpractice/previous/js/main.js
+++ basicUIpractice/previous/js/main.js
@@ -1,26 +1,27 @@
 "use strict"; 
 // アコーディオン
 {
-	const accordion_wrapper = document.querySelectorAll('.wrapper');
+	// const accordion_wrapper = document.querySelectorAll('.wrapper');
 
-	for (let i = 0; i < accordion_wrapper.length; i ++) {
-		accordion_wrapper[i].addEventListener('click', e => {
-			const target = event.target;
-			console.log(target);
-			const target_answer = target.querySelector('.wrapper-txt_a');
-			target_answer.classList.toggle('active');	
-		});
-	} 
-
-	// const accordion_wrappers = document.querySelectorAll('.wrapper');
-
-	// accordion_wrappers.forEach(clickedItem => {
-	// 	clickedItem.addEventListener('click', () => {
-	// 		console.log(clickedItem);
-	// 		const clickedItemAnswer = clickedItem.querySelector('.wrapper-txt_a');
-	// 		clickedItemAnswer.classList.toggle('active');	
+	// for (let i = 0; i < accordion_wrapper.length; i ++) {
+	// 	accordion_wrapper[i].addEventListener('click', e => {
+	// 		const target = event.target;
+	// 		console.log(target);
+	// 		const target_answer = target.querySelector('.wrapper-txt_a');
+	// 		console.log(target_answer);
+	// 		target_answer.classList.toggle('active');	
 	// 	});
-	// });
+	// } 
+
+	const accordion_wrappers = document.querySelectorAll('.wrapper');
+
+	accordion_wrappers.forEach(clickedItem => {
+		clickedItem.addEventListener('click', () => {
+			console.log(clickedItem);
+			const clickedItemAnswer = clickedItem.querySelector('.wrapper-txt_a');
+			clickedItemAnswer.classList.toggle('active');	
+		});
+	});
 
 }
 
--- basicUIpractice/smoothScroll/pattern1/js/main.js
+++ basicUIpractice/smoothScroll/pattern1/js/main.js
@@ -1,22 +1,11 @@
 'use strict';
 {
-	const header = document.getElementById('header');
-	const rect = header.getBoundingClientRect();
-	const scrollY = window.scrollY; // ページトップからのスクロール量
-	const headerOffsetTop = rect.top + scrollY; // rect.top : ビューポートにおけるページトップからの距離
-	console.log(scrollY);
-	console.log(headerOffsetTop);
+	const btn = document.getElementById('arrow');
 
-	//ウィンドウのスクロールイベントを監視
-	//スクロールするごとに処理を実行→window.scrollYの量はスクロールするたびに更新される
-	function onScrollHandler () {
-		if (window.scrollY > headerOffsetTop) {
-			header.classList.add('sticky');
-		} else {
-			header.classList.remove('sticky');
-		}
-	}
-
-	window.addEventListener('scroll', onScrollHandler);
-	
+	btn.addEventListener('click', () => {
+		window.scroll({
+			top: 0,
+			behavior: 'smooth'
+		});
+	});
 }
